2cb0b7f664e149084df6024a187e30c14226b172
qisen <2658763115@qq.com>
2022-12-21 21:10:15 +0800

    实现鼠标拖拽窗口，测试是否能从天气接口获取数据

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,6 +3,8 @@
 #include <QContextMenuEvent>
 #include <QAction>
 #include <QMenu>
+#include <QNetworkAccessManager>
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -22,6 +24,12 @@ MainWindow::MainWindow(QWidget *parent)
     connect(exitAction, QAction::triggered, this, [=]{
         qApp->exit(0);
     });
+
+    //关联获取到天气数据后的槽函数
+    networkAM = new QNetworkAccessManager(this);
+    connect(networkAM, &QNetworkAccessManager::finished, this,&MainWindow::Replied);
+
+    queryW("101010100");
 }
 
 MainWindow::~MainWindow()
@@ -29,11 +37,40 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::Replied(QNetworkReply *reply)
+{
+    qDebug() << "haohao";
+    QByteArray arr = reply->readAll();
+//    int status_code = reply->attribute(QNetworkRequest::HttpStatusCodeAttribute).toInt();
+    qDebug() << "" << arr.data();
+}
+
+//获取某个城市天气数据 http://t.weather.itboy.net/api/weather/city/
+void MainWindow::queryW(QString city)
+{
+    QUrl url("http://t.weather.itboy.net/api/weather/city/" + city);
+    networkAM->get(QNetworkRequest(url));
+}
+
+//右键菜单事件
 void MainWindow::contextMenuEvent(QContextMenuEvent *rightevent)
 {
     //点击右键弹出菜单
     exitMenu->exec(QCursor::pos());
     rightevent->accept();
+}
+
+void MainWindow::mousePressEvent(QMouseEvent *event)
+{
+    //获取偏移量
+    clickOffset = event->globalPos() - this->pos(); //鼠标点击的位置减去窗口左上角的位置
+
+}
+
+void MainWindow::mouseMoveEvent(QMouseEvent *event)
+{
+    //鼠标移动后设置窗口的位置
+    this->move(event->globalPos() - clickOffset);
 }
 
 
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QNetworkReply>
+#include <QNetworkAccessManager>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -18,13 +20,22 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    void Replied(QNetworkReply *reply);
+    void queryW(QString city);
 
     //右键退出所需
     QMenu * exitMenu;
     QAction* exitAction;
 
+    QPoint clickOffset;//点击拖拽时鼠标位置与窗口左上角的偏移量
+
+    QNetworkAccessManager* networkAM;
+
 protected:
     void contextMenuEvent(QContextMenuEvent *rightevent);
 
+    //实现鼠标点击拖动窗口
+    void mousePressEvent(QMouseEvent* event);//鼠标点击获取鼠标位置
+    void mouseMoveEvent(QMouseEvent* event);
 };
 #endif // MAINWINDOW_H
